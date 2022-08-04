9c4af0509260b5b8ea8cdde673c11c7a53df9f7a
Max Schlosser <mschlos3@hs-mittweida.de>
2022-08-04 11:32:14 +0200

    Fix jcifs dependency import

--- android/src/main/java/dev/schlosser/samba_browser/SambaBrowserPlugin.java
+++ android/src/main/java/dev/schlosser/samba_browser/SambaBrowserPlugin.java
@@ -24,6 +24,7 @@ public class SambaBrowserPlugin implements FlutterPlugin, MethodCallHandler {
 
   @Override
   public void onMethodCall(@NonNull MethodCall call, @NonNull Result result) {
+
     if (call.method.equals("getPlatformVersion")) {
       result.success("Android " + android.os.Build.VERSION.RELEASE);
     } else {
