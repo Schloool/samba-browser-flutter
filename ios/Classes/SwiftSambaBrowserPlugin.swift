6bbf41f6e95b824fb5c74d5f78dcf01e48ff3465
Max Schlosser <mschlos3@hs-mittweida.de>
2022-08-13 09:54:46 +0200

    Finish iOS file path parsing

--- ios/Classes/SwiftSambaBrowserPlugin.swift
+++ ios/Classes/SwiftSambaBrowserPlugin.swift
@@ -29,11 +29,8 @@ public class SwiftSambaBrowserPlugin: NSObject, FlutterPlugin {
     private func getShareList(args: [String:Any], flutterResult: @escaping FlutterResult) {
         
         // extract the first part of the URL to connect to the root-subfolder
-        var url: String = "smb://" + Array((args["url"] as! String).split(separator: "/"))[1..<3].joined(separator: "/")
-        if url.last == "/" {
-            url = String(url.dropLast())
-        }
-        
+        let url: String = "smb://" + Array((args["url"] as! String).split(separator: "/"))[1..<3].joined(separator: "/")
+
         // the last component of the url has to be extracted separately as the share string must not be empty
         let share: String = String(url.split(separator: "/").last!)
         let path: String = (args["url"] as! String).components(separatedBy: "/").dropFirst(4).joined(separator: "/")
@@ -54,20 +51,24 @@ public class SwiftSambaBrowserPlugin: NSObject, FlutterPlugin {
     
     private func saveFile(args: [String:Any], flutterResult: @escaping FlutterResult) {
         
-        let url: String = (args["url"] as! String).components(separatedBy: "/").dropLast().joined(separator: "/") + "/"
-        let share = url.components(separatedBy: "/").dropFirst(3).dropLast().joined(separator: "/")
-        let shareFileName: String = (args["url"] as! String).components(separatedBy: "/").last!
+        // extract the first part of the URL to connect to the root-subfolder
+        let url: String = "smb://" + Array((args["url"] as! String).split(separator: "/"))[1..<3].joined(separator: "/")
         
+        let share: String = String(url.split(separator: "/").last!)
+        let atPath: String = String((args["url"] as! String).replacingOccurrences(of: url, with: "").dropFirst())
+
         let saveFolder: String = NSHomeDirectory() + "/Documents/" //args["saveFolder"] as! String
         let fileName: String = args["fileName"] as! String
         let user: String = args["username"] as! String
         let password: String = args["password"] as! String
         
-        SMBClient(url: url, share: share, user: user, password: password).downloadFile(atPath: shareFileName, to: saveFolder + fileName, handler: { result in
+        SMBClient(url: url, share: share, user: user, password: password).downloadFile(atPath: atPath, to: saveFolder + fileName, handler: { result in
             switch result {
             case .success(let shares):
+                print("File downloaded successfully")
                 flutterResult(shares)
             case .failure(let error):
+                print(error)
                 flutterResult(FlutterError(code: "An error occurred", message: error.localizedDescription, details: nil))
             }
         })
